7e7653ac3044c2e4688f91a0b89697c050a6b816
Om-Ghosal <leor.dragneel7004@gmail.com>
2025-02-08 12:36:52 +0530

    ai text processing added

--- ai.py
+++ ai.py
@@ -1,20 +1,13 @@
 from langchain_core.prompts import ChatPromptTemplate,PromptTemplate
 from langchain_ollama.llms import OllamaLLM
 import re
+prompt_template = ChatPromptTemplate([
+    ('system','''Just fix any Gramtical errors, any spelling errors and add emotes where ever user asked for it.
+'''),
+    ('user',"{user}")
+])
 
-prompt_template = PromptTemplate.from_template('''Create an AI system that takes raw user input (e.g., dictated notes or thoughts) and processes it to produce clean, concise, and well-structured text. The system should handle corrections, remove filler words or phrases (e.g., 'no scratch that'), and incorporate relevant emojis or formatting to reflect the tone or intent of the user. For example:
-
-Input:
-'What is LangChain? no scratch that, what do you mean by Langchain? Is it a way for humanity to tap into the power of AI? thinking emote'
-
-Processed Output:
-'What do you mean by LangChain? Is it a way for humanity to tap into the power of AI? 🤔'
-
-The system should focus on natural language understanding, context retention, and user intent to deliver polished and meaningful output.'
-
-Now, process the following input text: {user}.''')
-
-model = OllamaLLM(model="deepseek-r1:8b")
+model = OllamaLLM(model="mistral")
 
 chain = prompt_template | model
 
--- preprocess_text.py
+++ preprocess_text.py
@@ -1,11 +1,12 @@
 import re
+from ai import text_preprocess,output_Cleaner
 
 def add_newline(text):
     temp_text = text.lower().replace("newline ", "\n")
     return temp_text
 
 def stopword_removal(text):
-    stopwords = ["the", "is", "example"] 
+    stopwords = ["scratch that","no no no","remove that","cut that out"] 
     pattern = rf".*?\b(?:{'|'.join(map(re.escape, stopwords))})\b.*\n?"
     result = re.sub(pattern, "", text).strip()
     result = re.sub(r'\n+', '\n', result)
@@ -16,7 +17,10 @@ def stopword_removal(text):
 
 
 if __name__ == "__main__":
-    text = """hello world.newline This is a test sentence.newline Another line before example stopword.newline Keep this line intact."""
+    text = """What is consciousness? No, wait scratch that. newline What do we really mean by being truly aware? Is it just neurons firing, or is there something deeper at play?newline Are we just biological machines processing inputs, or is there a spark of something more? thinking emote."""
 
     text = add_newline(text)
-    print(stopword_removal(text))
+    text= stopword_removal(text)
+    text = text_preprocess(text)
+
+    print(output_Cleaner(text))
